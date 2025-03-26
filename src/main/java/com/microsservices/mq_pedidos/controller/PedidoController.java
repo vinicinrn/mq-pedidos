ae37fb0da7bef2d0fd58354a555b3449747719ac
vinicinrn <zviniciusnascimento@gmail.com>
2025-03-26 17:29:59 -0300

    feat: pedido controller and mqrabbit template

---
+++ src/main/java/com/microsservices/mq_pedidos/controller/PedidoController.java
@@ -0,0 +1,37 @@
+package com.microsservices.mq_pedidos.controller;
+
+import com.microsservices.mq_pedidos.database.model.Pedido;
+import com.microsservices.mq_pedidos.service.PedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+@RequiredArgsConstructor
+public class PedidoController {
+
+    private final RabbitTemplate rabbitTemplate;
+    private PedidoService pedidoService;
+
+    @Value("${broker.queue.processamento.name}")
+    private String routingKey;
+
+    @PostMapping
+    public String criarPedido(@RequestBody Pedido pedido) {
+        Pedido pedidoSalvo = pedidoService.salvar(pedido);
+        rabbitTemplate.convertAndSend("", routingKey, pedidoSalvo.getDescricao()); //BROKER "spring.rabbitmq.addresses"
+
+        return "Pedido criado com sucesso, aguarde o processamento" + pedido.getDescricao();
+    }
+
+    @GetMapping
+    public List<Pedido> listarPedidos() {
+        return pedidoService.listarPedidos();
+    }
+
+}
