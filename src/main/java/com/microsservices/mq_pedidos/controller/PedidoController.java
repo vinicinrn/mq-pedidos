9ea779ab9e39bb47a68459016eb95e41dc1fcad4
vinicinrn <zviniciusnascimento@gmail.com>
2025-03-26 19:14:14 -0300

    feat(pedidos): ajuste do método salvarPedido e envio para fila de processamento

--- src/main/java/com/microsservices/mq_pedidos/controller/PedidoController.java
+++ src/main/java/com/microsservices/mq_pedidos/controller/PedidoController.java
@@ -5,7 +5,6 @@ import com.microsservices.mq_pedidos.service.PedidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +15,16 @@ import java.util.List;
 public class PedidoController {
 
     private final RabbitTemplate rabbitTemplate;
-    private PedidoService pedidoService;
+    private final PedidoService pedidoService;
 
     @Value("${broker.queue.processamento.name}")
     private String routingKey;
 
     @PostMapping
     public String criarPedido(@RequestBody Pedido pedido) {
-        Pedido pedidoSalvo = pedidoService.salvar(pedido);
-        rabbitTemplate.convertAndSend("", routingKey, pedidoSalvo.getDescricao()); //BROKER "spring.rabbitmq.addresses"
-
-        return "Pedido criado com sucesso, aguarde o processamento" + pedido.getDescricao();
+        Pedido pedidoSalvo = pedidoService.salvarPedido(pedido);
+        rabbitTemplate.convertAndSend("", routingKey, pedidoSalvo); //BROKER "spring.rabbitmq.addresses"
+        return "Pedido criado com sucesso, aguarde o processamento -  " + pedido.getDescricao();
     }
 
     @GetMapping
